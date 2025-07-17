974e48b74c8967e565a5945e238fcf8004042419
Jonathan Fricke <velolp05@gmail.com>
2025-07-17 14:29:54 +0200

    Add secret template to firmware

---
+++ firmware/include/secret_template.h
@@ -0,0 +1,11 @@
+#pragma once
+
+constexpr const char *SSID = "";
+constexpr const char *WIFI_PASSWORD = "";
+constexpr const char *SERVER_IP = "";
+constexpr const char *SERVER_POST_PATH = "/add_weight";
+constexpr const char *SERVER_GET_PATH = "/get_weight";
+constexpr int HTTPS_PORT = 8443;
+constexpr const char *API_KEY = "";
+
+constexpr const char *ROOT_CA = "";
